cf5d7d70485b4d33a49f3409ad4ba1de6c103c76
Swapnika Ravva <LasyaRavva>
2025-11-29 20:12:31 +0530

    admin

---
+++ admin.js
@@ -0,0 +1,4 @@
+let fleetData =[];
+const VECHICLE_IMAGE_SRC="https://coding-platform.s3.amazonaws.com/dev/lms/tickets/5e80fcb6-3f8e-480c-945b-30a5359eb40e/JNmYjkVr3WOjsrbu.png"
+
+const container = document.getElementById('fleetCardsContainer');
